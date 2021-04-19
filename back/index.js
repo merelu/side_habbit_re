0b867c4024ebd29a2a9c8317ef3cb99e828c2790
GYUHA_HWANG <gyuha0310@gmail.com>
2021-04-19 21:00:05 +0900

    mongoDB, express 연결

---
+++ back/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 5000;
+mongoose
+  .connect(
+    "mongodb+srv://gyuha:1234@react-boiler-plate.rwqey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
