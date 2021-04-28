063a37039d0be3b56c90d0c68b10643a44449f0c
GYUHA_HWANG <gyuha0310@gmail.com>
2021-04-28 23:15:38 +0900

    폴더 경로 셋팅

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { store } from "./store/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import { store } from "@store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
