eb6a06c27d75176f80dfb4df9e040141ffea8145
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-03 23:17:47 +0900

    login 페이지 작성

--- front/src/views/LoginPage/index.tsx
+++ front/src/views/LoginPage/index.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useAppDispatch } from "@store/hooks";
+import useInput from "@utils/useInput";
+import React, { useCallback } from "react";
+import { loginUser } from "@_actions/user_action";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const dispatch = useAppDispatch();
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmitHandler = useCallback(
+    (e) => {
+      e.preventDefault();
+      let body = {
+        email,
+        password,
+      };
+      dispatch(loginUser(body)).payload.then((response) => {
+        if (response.playload.loginSuccess) {
+        } else {
+          alert("Error");
+        }
+      });
+    },
+    [dispatch, email, password]
+  );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={onChangeEmail} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={onChangePassword} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
