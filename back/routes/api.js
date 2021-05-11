a435eb80fd0aafc1b6dcadc820549d785794fabb
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-11 19:30:40 +0900

    api 수정

--- back/routes/api.js
+++ back/routes/api.js
@@ -7,12 +7,9 @@ router.post("/users/register", (req, res, next) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err });
-
-    return res.status(200).json({
-      success: true,
-      userInfo,
-    });
+    if (err)
+      return res.status(401).json({ errorMessage: "아이디가 중복됩니다." });
+    return res.status(200).send("ok");
   });
 });
 
@@ -20,26 +17,26 @@ router.post("/users/login", (req, res, next) => {
   //요청된 이메일을 데이터 베이스에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다.",
-      });
+      return res
+        .status(401)
+        .json({ errorMessage: "제공된 이메일에 해당하는 유저가 없습니다." });
     }
     //요청한 이메일이 데이터 베이스에 있다면 비밀번호가 같은지 찾는다.
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch) {
-        return res.json({
-          loginSuccess: false,
-          message: "비밀번호가 틀렸습니다.",
-        });
+        return res.status(401).json({ errorMessage: "비밀번호가 틀렸습니다." });
       }
       //비밀번호가 맞다면 토큰을 생성한다.
       user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
-
+        if (err)
+          return res
+            .status(401)
+            .json({ errorMessage: "토큰생성에 실패했습니다." });
         res.cookie("x_auth", user.token).status(200).json({
-          loginSuccess: true,
-          userId: user._id,
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
         });
         // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
       });
@@ -68,16 +65,12 @@ router.get("/users/logout", auth, (req, res) => {
       token: "",
     },
     (err, user) => {
-      if (err) return res.json({ success: false, err });
+      if (err) return res.status(500).json({ errorMessage: "로그아웃 실패" });
       return res.status(200).send({
         success: true,
       });
     }
   );
-});
-
-router.get("/hello", (req, res) => {
-  res.send("안녕하세요~");
 });
 
 module.exports = router;
