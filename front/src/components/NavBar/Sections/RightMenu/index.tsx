8180726a1397b3b21c9f84df97625e559086d11f
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-16 17:21:26 +0900

    right menu 로그인 여부따라 전환 기능 추가

--- front/src/components/NavBar/Sections/RightMenu/index.tsx
+++ front/src/components/NavBar/Sections/RightMenu/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import { Menu } from "antd";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { logOutUser } from "@_actions/user_action";
 import { MenuMode } from "antd/lib/menu";
@@ -14,29 +14,28 @@ function RightMenu({ mode }: IRightMenu) {
   const logoutHandler = useCallback(() => {
     dispatch(logOutUser());
   }, [dispatch]);
-
-  // if (user.userData && !user.userData.isAuth) {
-  return (
-    <Menu mode={mode}>
-      <Menu.Item key="mail">
-        <a href="/login">Signin</a>
-      </Menu.Item>
-      <Menu.Item key="app">
-        <a href="/register">Signup</a>
-      </Menu.Item>
-    </Menu>
-  );
-  // } else {
-  //   return (
-  //     <Menu mode={mode}>
-  //       <Menu.Item key="logout">
-  //         <NavBarLink to="#" onClick={logoutHandler}>
-  //           Logout
-  //         </NavBarLink>
-  //       </Menu.Item>
-  //     </Menu>
-  //   );
-  // }
+  if (user.userData && !user.userData.isAuth) {
+    return (
+      <Menu mode={mode}>
+        <Menu.Item key="mail">
+          <Link to="/login">Signin</Link>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <Link to="/register">Signup</Link>
+        </Menu.Item>
+      </Menu>
+    );
+  } else {
+    return (
+      <Menu mode={mode}>
+        <Menu.Item key="logout">
+          <Link to="#" onClick={logoutHandler}>
+            Logout
+          </Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
 export default RightMenu;
