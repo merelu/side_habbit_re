9e3e322bf35e9ba337ea8cba1af554febc9bbd4a
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-16 17:20:44 +0900

    type 수정

--- front/src/typings/db.ts
+++ front/src/typings/db.ts
@@ -1,10 +1,10 @@
 export interface IUser {
-  _id: string;
-  isAuth: boolean;
-  isAdmin: boolean;
-  name: string;
-  email: string;
-  lastname: string;
-  role: boolean;
-  image: string;
+  _id?: string;
+  isAuth?: boolean;
+  isAdmin?: boolean;
+  name?: string;
+  email?: string;
+  lastname?: string;
+  role?: boolean;
+  image?: string;
 }
