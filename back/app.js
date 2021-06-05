13a62537bd850bbb319be013c49839b09db31163
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-05 21:29:29 +0900

    passport  및 production을 위한 lib 적용

---
+++ back/app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const cors = require("cors");
+const session = require("express-session");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const path = require("path");
+
+dotenv.config();
+
+const userRouter = require("./routes/user");
+const passportConfig = require("./passport");
+const connect = require("./models");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.set("PORT", port);
+//mongoose
+connect();
+
+passportConfig();
+
+const prod = process.env.NODE_ENV === "production";
+
+if (prod) {
+  app.enable("trust proxy");
+  app.use(morgan("combined"));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+  },
+};
+if (prod) {
+  sessionOption.cookie.secure = true;
+  sessionOption.cookie.proxy = true;
+}
+app.use(session(sessionOption));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/uploads", express.static("uploads"));
+
+//router
+app.use("/api/users", userRouter);
+
+app.listen(app.get("PORT"), () => {
+  console.log(`listening on port ${app.get("PORT")}`);
+});
