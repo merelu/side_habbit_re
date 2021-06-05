4ee6a76fe97693ef7cd917ef663244018a996680
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-05 21:27:34 +0900

    user model session 방식 로그인 방식으로 변환

--- back/models/User.js
+++ back/models/User.js
@@ -1,91 +1,28 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const dayjs = require("dayjs");
 
-const saltRounds = 10;
-
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    maxlength: 50,
+const { Schema } = mongoose;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 5,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    image: String,
   },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    minlength: 5,
-  },
-  role: {
-    type: Number,
-    default: 0,
-  },
-  image: String,
-  token: {
-    type: String,
-  },
-  tokenExp: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
-userSchema.pre("save", function (next) {
-  let user = this;
-  if (user.isModified("password")) {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      if (err) return next(err);
-
-      bcrypt.hash(user.password, salt, function (err, hash) {
-        if (err) return next(err);
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    next();
-  }
-});
-
-userSchema.methods.comparePassword = function (plainPassword, callback) {
-  //plainPassword db에는 암호화된 비밀번호가 있다.
-  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, isMatch);
-  });
-};
-
-userSchema.methods.generateToken = function (callback) {
-  //jsonwebtoken을 이용해서 token을 생성하기
-  let user = this;
-  let token = jwt.sign(user._id.toHexString(), "secretToken");
-  let onehour = dayjs().add(1, "hour").valueOf();
-  user.tokenExp = onehour;
-  user.token = token;
-  user.save(function (err, user) {
-    if (err) return callback(err);
-    callback(null, user);
-  });
-};
-
-userSchema.statics.findByToken = function (token, callback) {
-  let user = this;
-  //토큰을 decode 한다.
-  jwt.verify(token, "secretToken", function (err, decoded) {
-    // console.log(decode);
-    //유저 아이디를 이용해서 유저를 찾은 다음에
-    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-    user.findOne({ _id: decoded, token: token }, function (err, user) {
-      if (err) return callback(err);
-      callback(null, user);
-    });
-  });
-};
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = { User };
+module.exports = mongoose.model("User", userSchema);
