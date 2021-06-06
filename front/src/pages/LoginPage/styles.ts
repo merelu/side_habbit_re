c051fedb0e1e6bab12cdf147c4dee39a0bbb0bf2
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-06 17:01:20 +0900

    불필요한 파일 제거 repo

--- front/src/pages/LoginPage/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const ErrorText = styled.p`
-  color: #ff0000bf;
-  font-size: 0.7rem;
-  border: 1px solid;
-  padding: 1rem;
-  border-radius: "10px";
-`;
