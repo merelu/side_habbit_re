9bfaf0a4a513d69475c8e2d67f7a3e90bf3c9f29
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-11 00:19:27 +0900

    NavBar component 작성

---
+++ front/src/components/Navbar/index.tsx
@@ -0,0 +1,34 @@
+import { AppBar, Typography, Hidden, Toolbar } from "@material-ui/core";
+
+import React from "react";
+import { Link } from "react-router-dom";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import DrawerMenu from "../DrawerMenu";
+import { useNavBarStyles } from "./styles";
+
+const MainNavbar = () => {
+  const classes = useNavBarStyles();
+  return (
+    <>
+      <AppBar position="sticky" color="transparent">
+        <Toolbar className={classes.toobar}>
+          <Typography variant="h6" color="primary">
+            <Link className={classes.link} to="/">
+              Side_Habbit
+            </Link>
+          </Typography>
+          <Hidden smDown>
+            <Link to="/login">
+              <GitHubIcon />
+            </Link>
+          </Hidden>
+          <Hidden mdUp>
+            <DrawerMenu />
+          </Hidden>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default MainNavbar;
---
+++ front/src/components/Navbar/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useNavBarStyles = makeStyles({
+  toobar: {
+    justifyContent: "space-between",
+  },
+  link: {
+    textDecoration: "none",
+    color: "inherit",
+  },
+});
