d530bc5034c6b6500be3b2fdf080dcd27fe9b15c
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-11 01:53:51 +0900

    global style 적용 및 page 라우터 연결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,33 @@
+import { Container } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
+import { Route, Switch } from "react-router";
 import MainNavbar from "./components/Navbar";
+import GlobalStyles from "./GlobalStyles";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <MainNavbar />
+      <Container
+        maxWidth="lg"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "centers",
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </Container>
     </ThemeProvider>
   );
 }
