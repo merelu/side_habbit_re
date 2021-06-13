e7a98688cce3752b8f71960a2677d9f6be2da57d
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-13 21:20:58 +0900

    Navbar, DrawerMenu 로그인시 로그아웃시 UI 변경

--- front/src/components/Navbar/index.tsx
+++ front/src/components/Navbar/index.tsx
@@ -1,13 +1,30 @@
-import { AppBar, Typography, Hidden, Toolbar } from "@material-ui/core";
+import {
+  AppBar,
+  Typography,
+  Hidden,
+  Toolbar,
+  IconButton,
+} from "@material-ui/core";
 
-import React from "react";
+import React, { useCallback } from "react";
 import { Link } from "react-router-dom";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import DrawerMenu from "../DrawerMenu";
 import { useNavBarStyles } from "./styles";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { logoutUser } from "../../_actions/user_actions";
+import { push } from "connected-react-router";
 
 function MainNavbar() {
+  const dispatch = useAppDispatch();
   const classes = useNavBarStyles();
+  const { userData } = useAppSelector((state) => state.user);
+  const onClickLogout = useCallback(() => {
+    dispatch(logoutUser());
+    dispatch(push("/"));
+  }, [dispatch]);
+
   return (
     <>
       <AppBar position="sticky" color="transparent">
@@ -16,12 +33,18 @@ function MainNavbar() {
             <Link to="/">Side_Habbit</Link>
           </Typography>
           <Hidden smDown>
-            <Link to="/login">
-              <GitHubIcon />
-            </Link>
+            {userData ? (
+              <IconButton onClick={onClickLogout}>
+                <ExitToAppIcon />
+              </IconButton>
+            ) : (
+              <Link to="/login">
+                <GitHubIcon />
+              </Link>
+            )}
           </Hidden>
           <Hidden mdUp>
-            <DrawerMenu />
+            <DrawerMenu userData={userData} onClickLogout={onClickLogout} />
           </Hidden>
         </Toolbar>
       </AppBar>
