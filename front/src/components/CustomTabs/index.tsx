d4d62b7f8dfde16240c9d911a6ebe666ab77093b
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-15 19:49:08 +0900

    CustomTabs 컴포넌트 작성

---
+++ front/src/components/CustomTabs/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { Paper, Tabs, Tab, Divider } from "@material-ui/core";
+import AllInboxIcon from "@material-ui/icons/AllInbox";
+import FitnessCenterIcon from "@material-ui/icons/FitnessCenter";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import Tasks from "./Tasks";
+import EnhancedToolbar from "./EnhancedToolbar";
+import { data as initialData, IData } from "./Tasks/data";
+import { useCustomTapsStyles } from "./styles";
+
+export default function CustomTabs() {
+  const classes = useCustomTapsStyles();
+  const [value, setValue] = useState(0);
+  const [data, setData] = useState<IData[]>([]);
+  const handleChange = (e: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+  useEffect(() => {
+    if (value === 0) {
+      setData(initialData);
+    } else {
+      setData(initialData.filter((item) => item.type === value - 1));
+    }
+  }, [value]);
+  return (
+    <Paper className={classes.paper}>
+      <EnhancedToolbar />
+      <Divider />
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="primary"
+        textColor="primary"
+      >
+        <Tab icon={<AllInboxIcon />} label="All"></Tab>
+        <Tab icon={<FitnessCenterIcon />} label="Exercise"></Tab>
+        <Tab icon={<MenuBookIcon />} label="Study"></Tab>
+        <Tab icon={<SportsEsportsIcon />} label="Hobby"></Tab>
+      </Tabs>
+      <Divider />
+      <Tasks data={data} />
+    </Paper>
+  );
+}
---
+++ front/src/components/CustomTabs/styles.ts
@@ -0,0 +1,7 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+export const useCustomTapsStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: { width: "100%", margin: "50px 0", borderRadius: "15px 15px 0 0" },
+  })
+);
