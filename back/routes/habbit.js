5e4905752e5aae9f01104c94f987962c78295656
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-19 20:53:37 +0900

    habbit router create, get

--- back/routes/habbit.js
+++ back/routes/habbit.js
@@ -6,35 +6,40 @@ const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
 router.post("/create", isLoggedIn, (req, res, next) => {
-  const habbit = new Habbit({
-    ...req.body,
-    expiredDate: dayjs(req.body.expired),
-  });
+  const habbit = new Habbit(req.body);
 
   habbit.save((err, doc) => {
-    if (err) return res.status(401).send("습관을 저장하는데 실패했습니다.");
-    res.status(200).send("ok");
+    console.log(doc);
+    if (err)
+      return res
+        .status(401)
+        .json({ errorMessage: "습관을 저장하는데 실패했습니다." });
+    res.status(200).json({
+      successs: true,
+      habbit: doc,
+    });
   });
 });
 
 router.get(
-  "/habbits/getTodayHabbits/:userId/:date",
+  "/getTodayHabbits/:userId/:date",
   isLoggedIn,
   async (req, res, next) => {
     const habbits = await Habbit.find({
       writer: req.params.userId,
-      expiredDate: { $lte: dayjs(req.params.date) },
+      expiredDate: { $gte: dayjs(req.params.date) },
       createdAt: { $gte: dayjs(req.params.date) },
-    }).exec((err, habbits) => {
-      if (err) return res.status(401).send("습관을 가져오는데 실패했습니다.");
-      return habbits;
+    }).exec((err, values) => {
+      if (err)
+        return res
+          .status(401)
+          .json({ errorMessage: "습관을 가져오는데 실패했습니다." });
+      const habbits = values.filter(
+        (value) => value.schedule[dayjs().day()] === true
+      );
+
+      res.status(200).json({ success: true, habbits });
     });
-
-    habbits = await habbits.filter(
-      (habbit) => habbit.schedule.indexOf(dayjs(req.params.date).day()) === true
-    );
-
-    return res.status(200).json(habbits);
   }
 );
 module.exports = router;
