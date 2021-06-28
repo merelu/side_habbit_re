adc6d8dfbcdd9182c782a2f534dcfc97e6a32721
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-28 23:03:39 +0900

    commit 라우터 수정

--- back/routes/commit.js
+++ back/routes/commit.js
@@ -6,29 +6,31 @@ const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
 router.post("/pushCommited", isLoggedIn, (req, res, next) => {
-  const pushed = [];
-  req.body.forEach((item) => {
-    const commit = new Commit({
-      writer: item.writer,
-      habbitId: item.habbitId,
-      createAt: item.createAt,
+  Commit.insertMany(req.body, (err, docs) => {
+    if (err)
+      return res
+        .status(401)
+        .json({ errorMessage: `push failure habbitId:${item.habbitId}` });
+    return res.status(200).json({
+      successs: true,
+      pushed: docs,
     });
+  });
+});
 
-    commit.save((err, doc) => {
+router.get("/getTodayPushed/:date", isLoggedIn, (req, res, next) => {
+  Commit.find({ createAt: { $lte: dayjs(req.params.date).endOf("day") } })
+    .populate("habbitId", "title category")
+    .exec((err, docs) => {
       if (err)
         return res
           .status(401)
-          .json({ errorMessage: `push failure habbitId:${item.habbitId}` });
-      console.log(doc);
-      pushed.push(doc);
+          .json({ errorMessage: "습관을 가져오는데 실패했습니다." });
+      return res.status(200).json({
+        successs: true,
+        pushed: docs,
+      });
     });
-  });
-
-  console.log("asdf", pushed);
-  return res.status(200).json({
-    successs: true,
-    pushed: pushed,
-  });
 });
 
 module.exports = router;
