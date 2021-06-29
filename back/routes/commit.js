72171abd7e8f0c2706c9aea1405c4821e03761c5
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-29 22:03:06 +0900

    getTodayPushed 라우터 버그 수정 및 getAllPushed 라우터 추가

--- back/routes/commit.js
+++ back/routes/commit.js
@@ -19,18 +19,47 @@ router.post("/pushCommited", isLoggedIn, (req, res, next) => {
 });
 
 router.get("/getTodayPushed/:date", isLoggedIn, (req, res, next) => {
-  Commit.find({ createAt: { $lte: dayjs(req.params.date).endOf("day") } })
+  Commit.find({
+    createAt: {
+      $lte: dayjs(req.params.date).endOf("day"),
+      $gte: dayjs(req.params.date).startOf("day"),
+    },
+  })
     .populate("habbitId", "title category")
     .exec((err, docs) => {
       if (err)
         return res
           .status(401)
-          .json({ errorMessage: "습관을 가져오는데 실패했습니다." });
+          .json({ errorMessage: "금일 push한 습관이 없습니다." });
       return res.status(200).json({
         successs: true,
         pushed: docs,
       });
     });
 });
+
+router.get(
+  "/getPushedWithinRange/:startDate/:endDate",
+  isLoggedIn,
+  (req, res, next) => {
+    Commit.find({
+      createAt: {
+        $lte: dayjs(req.params.startDate).endOf("day"),
+        $gte: dayjs(req.params.endDate).startOf("day"),
+      },
+    })
+      .populate("habbitId", "title category")
+      .exec((err, docs) => {
+        if (err)
+          return res
+            .status(401)
+            .json({ errorMessage: "기간내 push한 commit이 없습니다." });
+        return res.status(200).json({
+          successs: true,
+          pushedAll: docs,
+        });
+      });
+  }
+);
 
 module.exports = router;
