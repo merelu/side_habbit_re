663830cc13ac67af4afbe1696752f688164e4059
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-30 21:00:48 +0900

    user id 불러오는 방식 변경, gettoday 관련 라우터 변경

--- back/routes/commit.js
+++ back/routes/commit.js
@@ -6,7 +6,7 @@ const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
 router.post("/pushCommited", isLoggedIn, (req, res, next) => {
-  Commit.insertMany(req.body, (err, docs) => {
+  Commit.insertMany({ ...req.body, writer: req.user._id }, (err, docs) => {
     if (err)
       return res
         .status(401)
@@ -18,31 +18,12 @@ router.post("/pushCommited", isLoggedIn, (req, res, next) => {
   });
 });
 
-router.get("/getTodayPushed/:date", isLoggedIn, (req, res, next) => {
-  Commit.find({
-    createAt: {
-      $lte: dayjs(req.params.date).endOf("day"),
-      $gte: dayjs(req.params.date).startOf("day"),
-    },
-  })
-    .populate("habbitId", "title category")
-    .exec((err, docs) => {
-      if (err)
-        return res
-          .status(401)
-          .json({ errorMessage: "금일 push한 습관이 없습니다." });
-      return res.status(200).json({
-        successs: true,
-        pushed: docs,
-      });
-    });
-});
-
 router.get(
   "/getPushedWithinRange/:startDate/:endDate",
   isLoggedIn,
   (req, res, next) => {
     Commit.find({
+      writer: req.user._id,
       createAt: {
         $lte: dayjs(req.params.startDate).endOf("day"),
         $gte: dayjs(req.params.endDate).startOf("day"),
--- back/routes/habbit.js
+++ back/routes/habbit.js
@@ -8,7 +8,7 @@ const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
 router.post("/create", isLoggedIn, (req, res, next) => {
-  const habbit = new Habbit(req.body);
+  const habbit = new Habbit({ ...req.body, writer: req.user._id });
 
   habbit.save((err, doc) => {
     if (err)
@@ -22,35 +22,32 @@ router.post("/create", isLoggedIn, (req, res, next) => {
   });
 });
 
-router.get(
-  "/getTodayHabbits/:userId/:date",
-  isLoggedIn,
-  async (req, res, next) => {
-    const commits = await Commit.find({
-      writer: req.params.userId,
-      createAt: { $lte: dayjs(req.params.date).endOf("day") },
-    }).exec();
+router.get("/getTodayHabbits", isLoggedIn, async (req, res, next) => {
+  const today = dayjs();
+  const commits = await Commit.find({
+    writer: req.user._id,
+    createAt: { $lte: today.endOf("day") },
+  }).exec();
 
-    await Habbit.find({
-      writer: req.params.userId,
-      expiredDate: {
-        $gte: dayjs(req.params.date),
-      },
-      [`schedule.${dayjs().day()}`]: true,
-    }).exec((err, docs) => {
-      if (err)
-        return res
-          .status(401)
-          .json({ errorMessage: "습관을 가져오는데 실패했습니다." });
-      const habbits = docs.filter(
-        (habbit) =>
-          commits.findIndex(
-            (commit) => commit.habbitId.toString() === habbit._id.toString()
-          ) < 0
-      );
+  await Habbit.find({
+    writer: req.user._id,
+    expiredDate: {
+      $gte: today.startOf("day"),
+    },
+    [`schedule.${today.day()}`]: true,
+  }).exec((err, docs) => {
+    if (err)
+      return res
+        .status(401)
+        .json({ errorMessage: "습관을 가져오는데 실패했습니다." });
+    const habbits = docs.filter(
+      (habbit) =>
+        commits.findIndex(
+          (commit) => commit.habbitId.toString() === habbit._id.toString()
+        ) < 0
+    );
 
-      return res.status(200).json({ success: true, habbits: habbits });
-    });
-  }
-);
+    return res.status(200).json({ success: true, habbits: habbits });
+  });
+});
 module.exports = router;
