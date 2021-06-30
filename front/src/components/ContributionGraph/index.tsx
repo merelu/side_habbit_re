478e6a5a62fadb153016d9cd6ce5a6e7bbcc0dac
GYUHA_HWANG <gyuha0310@gmail.com>
2021-06-30 21:02:41 +0900

    ContributionGraph 컴포넌트 완성

--- front/src/components/ContributionGraph/index.tsx
+++ front/src/components/ContributionGraph/index.tsx
@@ -1,9 +1,13 @@
 import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { Paper, Divider, Typography } from "@material-ui/core";
+import { ICommit } from "@typings/db";
 import { getPushedWithinRange } from "@_actions/commit_actions";
 import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
 import CalendarHeatmap from "react-calendar-heatmap";
 import ReactTooltip from "react-tooltip";
+import { useContributionGraphStyles } from "./styles";
+import { getPushed } from "@_reducers/commitSlice";
 
 const today = dayjs().toDate();
 
@@ -13,10 +17,27 @@ interface valueType {
 }
 
 function ContributionGraph() {
+  const classes = useContributionGraphStyles();
   const dispatch = useAppDispatch();
   const { pushedAll } = useAppSelector((state) => state.commit);
   const { userData } = useAppSelector((state) => state.user);
   const [values, setValues] = useState<valueType[]>([]);
+  const [maxCount, setMaxCount] = useState(0);
+  const generateValues = (arr: ICommit[]): valueType[] => {
+    return getRange(366).map((index) => {
+      const date = shiftDate(today, index);
+      const count = arr.filter((item) =>
+        dayjs(item.createAt).isSame(dayjs(date), "day")
+      ).length;
+      return {
+        date,
+        count,
+      };
+    });
+  };
+  const onClickGraph = (value: valueType) => {
+    dispatch(getPushed(dayjs(value.date).format("YYYY-MM-DD")));
+  };
 
   useEffect(() => {
     if (userData) {
@@ -31,62 +52,72 @@ function ContributionGraph() {
 
   useEffect(() => {
     if (pushedAll) {
-      setValues(
-        getRange(400).map((index) => {
-          const date = shiftDate(today, -index);
-          const count = pushedAll.filter((item) =>
-            dayjs(item.createAt).isSame(dayjs(date), "date")
-          ).length;
-          return {
-            date,
-            count,
-          };
-        })
-      );
+      setValues(generateValues(pushedAll));
     }
   }, [pushedAll]);
 
-  if (values.length > 0) {
-    return (
-      <div>
-        <CalendarHeatmap
-          startDate={shiftDate(today, -366)}
-          endDate={today}
-          values={values}
-          classForValue={(value: valueType) => {
-            if (!value) {
-              return "color-empty";
-            }
-            return `color-github-3`;
-          }}
-          tooltipDataAttrs={(value: valueType) => {
-            return {
-              "data-tip": `${dayjs(value.date).format(
-                "YYYY-MM-DD"
-              )} has count: ${value.count}`,
-            };
-          }}
-          showWeekdayLabels={true}
-          onClick={(value) =>
-            alert(`Clicked on value with count: ${value.count}`)
-          }
-        />
-        <ReactTooltip />
-      </div>
+  useEffect(() => {
+    setMaxCount(
+      Math.max.apply(
+        Math,
+        values.map((v) => v.count)
+      )
     );
-  } else {
-    return <div>없음</div>;
-  }
+  }, [values]);
+  return (
+    <Paper className={classes.paper}>
+      <Typography variant="h6" className={classes.title}>
+        Commited Habbits
+      </Typography>
+      <Divider />
+      {pushedAll && (
+        <div className={classes.graph}>
+          <CalendarHeatmap
+            startDate={shiftDate(today, 366)}
+            endDate={today}
+            values={values}
+            classForValue={(value: valueType) => {
+              if (value) {
+                if (value.count === 0) {
+                  return "color-empty";
+                }
+                return `color-github-${calColorSquare(value.count, maxCount)}`;
+              }
+            }}
+            tooltipDataAttrs={(value: valueType) => {
+              return {
+                "data-tip": `${dayjs(value.date).format(
+                  "YYYY-MM-DD"
+                )} has count: ${value.count}`,
+              };
+            }}
+            showWeekdayLabels={true}
+            onClick={(value) => onClickGraph(value)}
+          />
+        </div>
+      )}
+      <ReactTooltip />
+    </Paper>
+  );
 }
 
 function shiftDate(date: Date, numDays: number) {
-  const newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
-  return newDate;
+  const newDate = dayjs(date);
+  return newDate.subtract(numDays, "day").toDate();
 }
 
 function getRange(count: number) {
   return Array.from({ length: count }, (_, i) => i);
 }
 
+function calColorSquare(count: number, maxCount: number) {
+  const per = (count / maxCount) * 100;
+  if (per >= 75 && per <= 100) {
+    return 4;
+  } else if (per >= 50 && per < 75) {
+    return 3;
+  } else if (per >= 25 && per < 50) {
+    return 2;
+  } else if (per > 0 && per < 25) return 1;
+}
 export default ContributionGraph;
---
+++ front/src/components/ContributionGraph/styles.ts
@@ -0,0 +1,15 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+export const useContributionGraphStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: { width: "100%", margin: "50px 0", borderRadius: "15px 15px 0 0" },
+    graph: {
+      padding: `${theme.spacing(3)}px ${theme.spacing(3)}px ${theme.spacing(
+        1
+      )}px ${theme.spacing(3)}px`,
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+  })
+);
