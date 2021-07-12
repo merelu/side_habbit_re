9c0f29401393ef3f00af2c173ef086076769dff4
GYUHA_HWANG <gyuha0310@gmail.com>
2021-07-12 18:37:31 +0900

    dev setting

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -6,7 +6,10 @@ import { history, store } from "@store/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "react-calendar-heatmap/dist/styles.css";
+import axios from "axios";
 
+// axios.defaults.baseURL = "http://3.37.220.56:5000";
+// axios.defaults.baseURL = "http://localhost:5000";
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
