e18708869122479455153bb11bc2d2525bb7eb22
GYUHA_HWANG <gyuha0310@gmail.com>
2021-07-12 18:38:03 +0900

    image export 설정

--- front/src/image/index.ts
+++ front/src/image/index.ts
@@ -1,6 +1,23 @@
-export const imageData = [
+import card_add from "./card_add.svg";
+import card_grow from "./card_grow.svg";
+import card_todo from "./card_todo.svg";
+import card_complete from "./card_complete.svg";
+
+export const cardImageData = [
   {
-    img: "",
-    title: "heroSection",
+    img: card_add,
+    title: "info_card_add",
+  },
+  {
+    img: card_complete,
+    title: "info_card_todo",
+  },
+  {
+    img: card_todo,
+    title: "info_card_todo",
+  },
+  {
+    img: card_grow,
+    title: "info_card_grow",
   },
 ];
